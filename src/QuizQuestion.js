d7a16d7b6c5233d9b784e65ed33f0a890f7983f5
sp-stuart <sathyapstuart@gmail.com>
2020-02-02 19:52:09 -0600

    6.1 - quiz question handler

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -3,6 +3,11 @@ import QuizQuestionButton from './QuizQuestionButton.js'
 
 class QuizQuestion extends Component {
 
+    handleClick(buttonText) {
+        if(buttonText === this.props.quiz_question.answer) {
+            this.props.showNextQuestionHandler()
+        }
+    }
     render() {
         return (
             <main>
@@ -13,7 +18,10 @@ class QuizQuestion extends Component {
                 </section>
                 <section className="buttons">
                     <ul>
-                        <QuizQuestionButton button_text={this.props.quiz_question.answer_options[0]} />
+                        {this.props.quiz_question.answer_options.map((option, index) => {
+                            return <QuizQuestionButton key={index} button_text={option} 
+                            clickHandler={this.handleClick.bind(this)} />
+                        })}
                     </ul>
                 </section>
             </main>
