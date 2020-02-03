597d7bb0e768770282096440976c8571b91f5ffe
sp-stuart <sathyapstuart@gmail.com>
2020-02-02 20:04:07 -0600

    7.1 - quiz answer validation

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -8,7 +8,7 @@ class QuizQuestion extends Component {
     }
     handleClick(buttonText) {
         if (buttonText === this.props.quiz_question.answer) {
-            this.state.incorrectAns = false;
+            this.setState({incorrectAns : false});
             this.props.showNextQuestionHandler()
         } else {
             this.setState({incorrectAns : true});
