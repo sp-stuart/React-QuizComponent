2c2a3c4b850ff2cb8135d8409b817000462e1b7f
sp-stuart <sathyapstuart@gmail.com>
2020-02-02 20:00:51 -0600

    7.1 - quiz answer validation

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -2,10 +2,16 @@ import React, { Component } from 'react';
 import QuizQuestionButton from './QuizQuestionButton.js'
 
 class QuizQuestion extends Component {
-
+    constructor(props) {
+        super(props);
+        this.state = { incorrectAns: false };
+    }
     handleClick(buttonText) {
-        if(buttonText === this.props.quiz_question.answer) {
+        if (buttonText === this.props.quiz_question.answer) {
+            this.state.incorrectAns = false;
             this.props.showNextQuestionHandler()
+        } else {
+            this.setState({incorrectAns : true});
         }
     }
     render() {
@@ -19,11 +25,12 @@ class QuizQuestion extends Component {
                 <section className="buttons">
                     <ul>
                         {this.props.quiz_question.answer_options.map((option, index) => {
-                            return <QuizQuestionButton key={index} button_text={option} 
-                            clickHandler={this.handleClick.bind(this)} />
+                            return <QuizQuestionButton key={index} button_text={option}
+                                clickHandler={this.handleClick.bind(this)} />
                         })}
                     </ul>
                 </section>
+                {this.state.incorrectAns ? <p className='error'>Sorry, incorrect answer!</p> : null}
             </main>
         );
     }
