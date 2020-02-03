40e5c793d4f9d21bc89e839b481b6337728cc51f
sp-stuart <sathyapstuart@gmail.com>
2020-02-02 20:10:17 -0600

    7 - final quiz ans validation

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -4,14 +4,14 @@ import QuizQuestionButton from './QuizQuestionButton.js'
 class QuizQuestion extends Component {
     constructor(props) {
         super(props);
-        this.state = { incorrectAns: false };
+        this.state = { incorrectAnswer: false };
     }
     handleClick(buttonText) {
         if (buttonText === this.props.quiz_question.answer) {
-            this.setState({incorrectAns : false});
+            this.setState({incorrectAnswer : false});
             this.props.showNextQuestionHandler()
         } else {
-            this.setState({incorrectAns : true});
+            this.setState({incorrectAnswer : true});
         }
     }
     render() {
@@ -30,7 +30,7 @@ class QuizQuestion extends Component {
                         })}
                     </ul>
                 </section>
-                {this.state.incorrectAns ? <p className='error'>Sorry, that's not right</p> : null}
+                {this.state.incorrectAnswer ? <p className='error'>Sorry, that's not right</p> : null}
             </main>
         );
     }
